78beea63270e4c7d951193462a4fd9a4fbcc008c
Philzen <philzen@riseup.net>
2023-10-21 20:08:02 +0200

    Remove all types unrelated to the bug

--- example-rhf.d.ts
+++ example-rhf.d.ts
@@ -2,25 +2,6 @@
 /// <reference lib="ES2015" />
 import type { RegisterOptions } from 'react-hook-form'
 
-interface InterviewParticipantInfo {
-  type: 'string' | 'email'
+export interface TsJsonGeneratorBug {
   validation: RegisterOptions
-}
-
-type ParticipantInfoKey = string
-
-interface ParticipantInfoObject {
-  [key: ParticipantInfoKey]: InterviewParticipantInfo
-}
-
-export interface InterviewToolConfig {
-  /**
-   * ParticipantInfoObject:
-   * Add a key for every data itemyou want to capture. The keys
-   * will show up in all subsequent data exports, i.e. in the
-   * payload of webhooks.
-   *
-   * @example "firstName": { type: "string" }
-   */
-  participantInfo: ParticipantInfoObject
 }
